18a8a44c6efdf04ab662766167809b86efaeaf88
Nitish Sanghi <nitishsanghi@Nitishs-MacBook-Pro-2.local>
2021-09-24 00:17:44 -0700

    ROS repo

---
+++ Robot Ball Chaser/catkin_ws/src/ball_chaser/src/drive_bot.cpp
@@ -0,0 +1,54 @@
+#include "ros/ros.h"
+#include "geometry_msgs/Twist.h"
+#include "ball_chaser/DriveToTarget.h"
+#include <std_msgs/Float64.h>
+#include <string>
+#include <sstream>
+
+// ROS::Publisher motor commands;
+ros::Publisher motor_command_publisher;
+
+bool handle_drive_request(ball_chaser::DriveToTarget::Request& req, ball_chaser::DriveToTarget::Response& res){
+
+	ROS_INFO("DriveToTargetRequest received -j1:%1.2f, j2:%1.2f", (float)req.linear_x, (float)req.angular_z);
+
+	// Create a motor_command object of type geometry_msgs::Twist
+	geometry_msgs::Twist motor_command;
+	// Set wheel velocities, forward [0.5, 0.0]
+	motor_command.linear.x = req.linear_x;
+	motor_command.angular.z = req.angular_z;
+	// Publish angles to drive the robot
+	motor_command_publisher.publish(motor_command);
+
+	std::ostringstream s1;
+	std::ostringstream s2;
+	s1<<req.linear_x;
+	s2<<req.angular_z;
+	//res.msg_feedback = "Joint angles set - j1: " + std::to_string(req.linear_x) + " , j2: " + std::to_string(req.angular_z);
+	res.msg_feedback = "Joint angles set - j1: " + s1.str() + " , j2: " + s2.str();
+	ROS_INFO_STREAM(res.msg_feedback);
+
+	return true;
+
+}
+
+
+int main(int argc, char** argv)
+{
+	// Initialize a ROS node
+	ros::init(argc, argv, "drive_bot");
+
+	// Create a ROS NodeHandle object
+	ros::NodeHandle n;
+
+	// Inform ROS master that we will be publishing a message of type geometry_msgs::Twist on the tobot actuation topic with a publishing queue size of 10
+	motor_command_publisher = n.advertise<geometry_msgs::Twist>("/cmd_vel",10);
+
+	ros::ServiceServer service = n.advertiseService("/ball_chaser/command_robot",handle_drive_request);
+
+	ROS_INFO("Ready to send joint commands");
+
+	ros::spin();
+
+	return 0;
+}
