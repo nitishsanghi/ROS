480775411eecbaf21c0bb9769a95b18ee5542b60
NitishSanghi <nitishsanghi@gmail.com>
2022-01-18 20:58:34 -0800

    Delete HomeServiceRobot/catkin_ws directory

--- HomeServiceRobot/catkin_ws/build/pick_objects/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/workspace/catkin_ws/src/pick_objects/include".split(';') if "/home/workspace/catkin_ws/src/pick_objects/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "actionlib;move_base_msgs;roscpp".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lpick_objects".split(';') if "-lpick_objects" != "" else []
-PROJECT_NAME = "pick_objects"
-PROJECT_SPACE_DIR = "/home/workspace/catkin_ws/devel"
-PROJECT_VERSION = "0.0.0"
